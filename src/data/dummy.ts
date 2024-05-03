1b84ecf28dc8fe5dbfa4e6ccd06bd6b3260be7d6
emmanuelakdg <akdgemman@gmail.com>
2024-05-03 11:35:40 +0000

    add apexcharts

--- src/data/dummy.ts
+++ src/data/dummy.ts
@@ -1,166 +1,165 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-
 export const USER_COLUMS = [
-    {
-        label: '',
-        sort: (data: any[], sens: boolean) => {
-            return data
-        }
+  {
+    label: "",
+    sort: (data: any[], sens: boolean) => {
+      return data;
     },
-    {
-        label: 'name',
-        sort: (data: any[], sens: boolean) => {
-            return data.sort((a, b) => {
-                if (sens) {
-                    return a.name > b.name ? 1 : -1
-                } else {
-                    return a.name < b.name ? 1 : -1
-                }
-            })
+  },
+  {
+    label: "name",
+    sort: (data: any[], sens: boolean) => {
+      return data.sort((a, b) => {
+        if (sens) {
+          return a.name > b.name ? 1 : -1;
+        } else {
+          return a.name < b.name ? 1 : -1;
         }
+      });
     },
-    {
-        label: 'email',
-        sort: (data: any[], sens: boolean) => {
-            return data.sort((a, b) => {
-                if (sens) {
-                    return a.email > b.email ? 1 : -1
-                } else {
-                    return a.email < b.email ? 1 : -1
-                }
-            })
+  },
+  {
+    label: "email",
+    sort: (data: any[], sens: boolean) => {
+      return data.sort((a, b) => {
+        if (sens) {
+          return a.email > b.email ? 1 : -1;
+        } else {
+          return a.email < b.email ? 1 : -1;
         }
+      });
     },
-    {
-        label: 'role',
-        sort: (data: any[], sens: boolean) => {
-            return data.sort((a, b) => {
-                if (sens) {
-                    return a.role > b.role ? 1 : -1
-                } else {
-                    return a.role < b.role ? 1 : -1
-                }
-            })
+  },
+  {
+    label: "role",
+    sort: (data: any[], sens: boolean) => {
+      return data.sort((a, b) => {
+        if (sens) {
+          return a.role > b.role ? 1 : -1;
+        } else {
+          return a.role < b.role ? 1 : -1;
         }
+      });
     },
-    {
-        label: 'status',
-        sort: (data: any[], sens: boolean) => {
-            return data.sort((a, b) => {
-                if (sens) {
-                    return a.status > b.status ? 1 : -1
-                } else {
-                    return a.status < b.status ? 1 : -1
-                }
-            })
+  },
+  {
+    label: "status",
+    sort: (data: any[], sens: boolean) => {
+      return data.sort((a, b) => {
+        if (sens) {
+          return a.status > b.status ? 1 : -1;
+        } else {
+          return a.status < b.status ? 1 : -1;
         }
+      });
     },
-    {
-        label: 'action',
-        sort: (data: any[], sens: boolean) => {
-            return data
-        }
-    }
-]
+  },
+  {
+    label: "action",
+    sort: (data: any[], sens: boolean) => {
+      return data;
+    },
+  },
+];
 
 export const USER_DATA = [
-    {
-        name: 'user1',
-        email: '',
-        role: 'admin',
-        status: 'active',
-        products: ['product1', 'product2', 'product3']
-    },
-    {
-        name: 'user2',
-        email: '',
-        role: 'user',
-        status: 'active',
-        products: ['product1', 'product2', 'product3']
-    },
-    {
-        name: 'user3',
-        email: '',
-        role: 'user',
-        status: 'active',
-        products: ['product1', 'product2', 'product3']
-    },
-    {
-        name: 'user4',
-        email: '',
-        role: 'user',
-        status: 'active',
-        products: ['product1', 'product2', 'product3']
-    },
-    {
-        name: 'user5',
-        email: '',
-        role: 'user',
-        status: 'active',
-        products: ['product1', 'product2', 'product3']
-    },
-    {
-        name: 'user6',
-        email: '',
-        role: 'user',
-        status: 'active',
-        products: ['product1', 'product2', 'product3']
-    },
-    {
-        name: 'user7',
-        email: '',
-        role: 'user',
-        status: 'active',
-        products: ['product1', 'product2', 'product3']
-    },
-    {
-        name: 'user8',
-        email: '',
-        role: 'user',
-        status: 'active',
-        products: ['product1', 'product2', 'product3']
-    },
-    {
-        name: 'user9',
-        email: '',
-        role: 'user',
-        status: 'active',
-        products: ['product1', 'product2', 'product3']
-    },
-    {
-        name: 'user10',
-        email: '',
-        role: 'user',
-        status: 'active',
-        products: ['product1', 'product2', 'product3']
-    },
-    {
-        name: 'user11',
-        email: '',
-        role: 'user',
-        status: 'active',
-        products: ['product1', 'product2', 'product3']
-    },
-    {
-        name: 'user12',
-        email: '',
-        role: 'user',
-        status: 'active',
-        products: ['product1', 'product2', 'product3']
-    },
-    {
-        name: 'user13',
-        email: '',
-        role: 'user',
-        status: 'active',
-        products: ['product1', 'product2', 'product3']
-    },
-    {
-        name: 'user14',
-        email: '',
-        role: 'user',
-        status: 'active',
-        products: ['product1', 'product2', 'product3']
-    }
-]
+  {
+    name: "user1",
+    email: "",
+    role: "admin",
+    status: "active",
+    products: ["product1", "product2", "product3"],
+  },
+  {
+    name: "user2",
+    email: "",
+    role: "user",
+    status: "active",
+    products: ["product1", "product2", "product3"],
+  },
+  {
+    name: "user3",
+    email: "",
+    role: "user",
+    status: "active",
+    products: ["product1", "product2", "product3"],
+  },
+  {
+    name: "user4",
+    email: "",
+    role: "user",
+    status: "active",
+    products: ["product1", "product2", "product3"],
+  },
+  {
+    name: "user5",
+    email: "",
+    role: "user",
+    status: "active",
+    products: ["product1", "product2", "product3"],
+  },
+  {
+    name: "user6",
+    email: "",
+    role: "user",
+    status: "active",
+    products: ["product1", "product2", "product3"],
+  },
+  {
+    name: "user7",
+    email: "",
+    role: "user",
+    status: "active",
+    products: ["product1", "product2", "product3"],
+  },
+  {
+    name: "user8",
+    email: "",
+    role: "user",
+    status: "active",
+    products: ["product1", "product2", "product3"],
+  },
+  {
+    name: "user9",
+    email: "",
+    role: "user",
+    status: "active",
+    products: ["product1", "product2", "product3"],
+  },
+  {
+    name: "user10",
+    email: "",
+    role: "user",
+    status: "active",
+    products: ["product1", "product2", "product3"],
+  },
+  {
+    name: "user11",
+    email: "",
+    role: "user",
+    status: "active",
+    products: ["product1", "product2", "product3"],
+  },
+  {
+    name: "user12",
+    email: "",
+    role: "user",
+    status: "active",
+    products: ["product1", "product2", "product3"],
+  },
+  {
+    name: "user13",
+    email: "",
+    role: "user",
+    status: "active",
+    products: ["product1", "product2", "product3"],
+  },
+  {
+    name: "user14",
+    email: "",
+    role: "user",
+    status: "active",
+    products: ["product1", "product2", "product3"],
+  },
+];
